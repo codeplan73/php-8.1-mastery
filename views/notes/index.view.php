28b094ab2835b0534049b2f387e0e12a10c4b8ef
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2023-07-18 17:55:18 +0100

    optimize the views directory

---
+++ views/notes/index.view.php
@@ -0,0 +1,21 @@
+<?php require('views/partials/head.php') ?>
+<?php require('views/partials/nav.php') ?>
+<?php require('views/partials/banner.php') ?>
+
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <?php foreach ($notes as $note): ?>
+            <li>
+                <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
+                    <?= htmlspecialchars($note['body']) ?> </a>
+            </li>
+        <?php endforeach; ?>
+
+        <div class="my-6">
+            <a class="text-blue-500 font-bold hover:underline" href="/notes/create">Create Note</a>
+        </div>
+    </div>
+
+</main>
+
+<?php require('views/partials/footer.php') ?>

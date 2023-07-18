ed74282e1503699eb99e018b28d0515a443f1902
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2023-07-18 17:38:55 +0100

    handle validation and input sanization

--- views/notes.view.php
+++ views/notes.view.php
@@ -5,13 +5,17 @@
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <?php foreach ($notes as $note): ?>
-            <li>
-                <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
-                    <?= $note['body'] ?>
-                </a>
-            </li>
+        <li>
+            <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
+                <?= htmlspecialchars($note['body']) ?> </a>
+        </li>
         <?php endforeach; ?>
+
+        <div class="my-6">
+            <a class="text-blue-500 font-bold hover:underline" href="/notes/create">Create Note</a>
+        </div>
     </div>
+
 </main>
 
 <?php require('partials/footer.php') ?>
